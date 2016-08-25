e3a4f4bdf9e2324b9bd2fe43e27511245e8638f5
Erin Feichtinger <efeichtinger@mail.usf.edu>
2016-08-25 10:41:35 -0400

    Revised script with function for integers and the start of the program for matching characters

--- PericeAug24.R
+++ PericeAug24.R
@@ -1,17 +1,12 @@
-library(stringr)
-#str_subset()
+#Peirce August 24 2016
 
+
+#Function for integers only 
 Peirce =  function(DF)
 {
   
-  ## EF 8/11/2016 - not convinced on this line as it is 
-  ## What if we have non-numeric data? How can this approach work?
-  ## Seems like it only works on numeric data, can't work on twitter data
   check.numeric <- function(N){
     !length(grep("[^[:digit:]]", as.character(N)))
-    
-    #return logical, T or F for matching the reg expression digit
-    # 
   }
   
   
@@ -20,12 +15,11 @@ Peirce =  function(DF)
   col3 = DF[,3];
   
   if(check.numeric(col1))
-  {                        #ecdf returns a function for a dist'n
-                           #why call col twice?
+  {
     DF$percentageofx = ecdf(col1)(col1);
   }
   
-  if(check.numeric(col2))       #need else statements to deal with characters
+  if(check.numeric(col2))
   {
     DF$percentageofy = ecdf(col2)(col2);
   }
@@ -99,53 +93,58 @@ Peirce =  function(DF)
     
     
   }
-}
   
- 
-  ##Are comparing across columns or rows? Are we looking at matches in
-  #the dist'n at position 3 (object)?
+  
   
   ##Creates a percentage rank of values based on the column they reside in
   
-  #What about other summary outputs?
+  
   
   View(DF);
   
   ##Reading the data column, 50% means a distribution that is very close to average.
+  ## EF so no trend if near 50%?
+}
+
+#Integers - example made on purpose to have similar values across rows
+x <- c(0,1,0,1,1,1,0,0,1,0)
+y <- c(95,3,97,5,4,4,98,97,4,98)
+z <- c(3,10,2,10,11,11,2,3,10,2)
+df <- data.frame(x,y,z)
+test.int <- Peirce(df)
+
+
+x1 <- floor(runif(20, min=0, max=101))
+y1 <- floor(runif(20, min=0, max=101))
+z1 <- floor(runif(20, min=0, max=101))
+df1 <- data.frame(x1,y1,z1)
+test2 <- Peirce(df1)
+
+
+#######################################################
+#Made up character strings for testing character function
+x2 <- c("cat","cat","cat","dog","dog","dog")
+y2 <- rep(LETTERS[1:6],1)
+z3 <- c("a","b","c","d","e","f")
+df2 <- data.frame(x,y,z)
+
+Peirce.char = function(DF){
+  
+  
+  
+  col1 = DF[,1];
+  col2 = DF[,2];  #what's with the semi-colons?
+  col3 = DF[,3]; 
+  
+  for (k in 1:nrow(DF)){    #want to construct a loop to make each row a 
+    str.vec <- c(col1, col2, col3) #character vector 
+  }  
+  
+  for (i in 1:nrow(DF)){
+    
+    grep(i,1:nrow(DF))
+  }
   
 }
 
-## Need to extend function for character vectors but I don't see how
-## we could use ecdf for this 
-## The twitter data set couldn't be classified using this
-
-## else statements 
-
-
-#test
-a <- sample(1:10, 10, replace=TRUE)
-b <- sample(90:100, 10, replace=TRUE)
-c <- sample(90:100, 10, replace=TRUE)
-
-aa <- sample(1:1, 10, replace=TRUE)
-bb <- sample(1:1, 10, replace=TRUE)
-cc <- sample(0.1:1, 10, replace=TRUE)
-
-dff <- data.frame(a,b,c)
-test <- Peirce(dff)
-
-d <- data.frame(aa,bb,cc)
-test2 <- Peirce(d)
-#Right should be all 100% since they are the same 
-
-aaa <- c(0,1,0,1,1,1,0,0,1,0)
-bbb <- c(95,3,97,5,4,4,98,97,4,98)
-ccc <- c(3,10,2,10,11,11,2,3,10,2)
-dd <- data.frame(aaa,bbb,ccc)
-test3 <- Peirce(dd)
-
-
-#### Testing some functions for string matching
-#making use of regular expressions
-
-
+###%in%
