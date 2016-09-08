f93eb9e770cc006c4bc197975931128e52ba7b9f
Erin Feichtinger <efeichtinger@mail.usf.edu>
2016-09-08 14:01:02 -0400

    Minor adjustments to twitter data headers

--- PericeAug24.R
+++ PericeAug24.R
@@ -2,6 +2,7 @@
 
 library(stringr)
 library(tidyr)
+library(plyr)
 
 #Function for integers only 
 Peirce =  function(DF)
@@ -108,6 +109,8 @@ Peirce =  function(DF)
   ## EF so no trend if near 50%?
 }
 
+
+
 #Integers - example made on purpose to have similar values across rows
 x <- c(0,1,0,1,1,1,0,0,1,0)
 y <- c(95,3,97,5,4,4,98,97,4,98)
@@ -130,14 +133,79 @@ y2 <- rep(LETTERS[1:6],1)
 z3 <- c("a","b","c","d","e","f")
 df2 <- data.frame(x,y,z)
 
-require(tidyr)
+twitter <- read.csv("Twitter_Peirce.csv")
+class(twitter)
+head(twitter)
+str(twitter)
 
-              
-               
-Peirce.char = function(DF){
-  #
-  data <- DF %>% unite(strvec, 1, 2, 3, sep = ",")
+
+
+
+#function from N Matloff book pg 92
+findwords <- function(tf) {
+  txt<- scan(tf,"")
+  wl <- list()
+  for(i in 1:length(txt)){
+    wrd <- txt[i]
+    wl[[wrd]] <- c(wl[[wrd]],i)
+    
+  }
+  return(wl)
   
- 
+}
+
+test.output <- findwords("Twitter_Peirce2.txt")
+length(test.output)
+#139 - words in data set 
+
+#returns name and the place in list
+test.output[10]
+test.output[[10]]
+
+
+#function for summing the words classified under each name
+#append one of three classes for the words - sign, object, interp
+#how to do this?
+
+count.freq <- function(x){
+  mats <- list()
+  for(i in 1:length(x)){
+    
+  }
+}
+
+
+#but specific to these data
+#pattern matching to "Mentions", "Tweets", "Replies to", "None"
+#class "interpretant"
+
+class(intrp) <- "intrp"
+
+#function for finding list components,input x = list
+find.intrp <-
+  for (i in 1:length(x))
+    #grep()
+}
+
+
+                            
+Peirce.char = function(DF){
+  #creates a data frame of 1 column and n rows 
+  #each row has three components, obs from col1,2,and 3
+  data <- DF %>% unite(strvec, 1, 2, 3, sep = ",")
+  data[,"index"] <- c(rownames(data))
+  return(data)
+  
+  #now need to loop through each row and compare it to all other rows 
+    for(i in 1:nrow(data)) {
+    triad <- data[i,]
+    grep("^[[:alpha:]]",triad) #the problem is I don't know what to tell
+                                #r to look for, stringr looks for regex
+                                #I want it to look for any sequence characters
+    
+  }
+  
+}
+
 
 ###%in%
